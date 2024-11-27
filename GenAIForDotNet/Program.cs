65e86d9cc349650ebe531f2adf6ed95b249eca6b
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2024-11-27 23:15:30 +0300

    02_06_End finished.

--- GenAIForDotNet/Program.cs
+++ GenAIForDotNet/Program.cs
@@ -8,13 +8,20 @@ var configuration = new ConfigurationBuilder()
            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
            .Build();
 
-string key = configuration["Azure:ApiKey"];
+string? key = configuration["Azure:ApiKey"];
+if(string.IsNullOrEmpty(key))
+{
+    Console.WriteLine("Please set the OPENAI_API_KEY environment variable.");
+    return;
+}
+
 string deploymentName = configuration["Azure:DeploymentName"];
 
 var client = new OpenAIClient(key);
 
 ChatCompletionsOptions completionsOptions = new();
 completionsOptions.DeploymentName = deploymentName;
+// completionsOptions.MaxTokens = 5; error handling
 
 // System message
 ChatRequestSystemMessage systemMsg =
@@ -23,24 +30,46 @@ ChatRequestSystemMessage systemMsg =
         conference in Seattle. Be brief.");
 completionsOptions.Messages.Add(systemMsg);
 
-while (true)
+try
 {
-    // AI's answer
-    Console.ForegroundColor = ConsoleColor.White;
-    var result = client.GetChatCompletions(completionsOptions);
-    var firstChoice = result.Value.Choices.FirstOrDefault();
-    var answer = firstChoice?.Message?.Content;
+    while (true)
+    {
+        // AI's answer
+        Console.ForegroundColor = ConsoleColor.White;
+        var result = client.GetChatCompletions(completionsOptions);
+        var firstChoice = result?.Value?.Choices?.FirstOrDefault();
+        if(firstChoice == null)
+        {
+            Console.WriteLine("No chat completion choices available.");
+            break;
+        }
 
-    // Answer added to the Chat History
-    completionsOptions.Messages.Add(
-        new ChatRequestAssistantMessage(answer));
+        if(firstChoice.FinishReason != CompletionsFinishReason.Stopped)
+        {
+            Console.WriteLine($"Chat completion didn't finish normally."  +
+                              $"Reason : {firstChoice.FinishReason}");
+            break;
+        }
 
-    Console.WriteLine(answer);
-    Console.WriteLine();
+        var answer = firstChoice?.Message?.Content;
 
-    // User's Input
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    var userInput = Console.ReadLine();
-    ChatRequestUserMessage userMessage = new(userInput);
-    completionsOptions.Messages.Add(userMessage);
+        // Answer added to the Chat History
+        completionsOptions.Messages.Add(
+            new ChatRequestAssistantMessage(answer));
+
+        Console.WriteLine(answer);
+        Console.WriteLine();
+
+        // User's Input
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        var userInput = Console.ReadLine();
+        ChatRequestUserMessage userMessage = new(userInput);
+        completionsOptions.Messages.Add(userMessage);
+    }
 }
+catch (Exception ex)
+{
+    Console.WriteLine($"An error has occured:\n{ex.Message}");
+    throw;
+}
+
