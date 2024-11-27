e8a77c85e0d8376cca2db8fb244c49e15ee45c3c
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2024-11-27 22:47:58 +0300

    asd

--- GenAIForDotNet/Program.cs
+++ GenAIForDotNet/Program.cs
@@ -3,10 +3,9 @@ using System.IO;
 using Microsoft.Extensions.Configuration;
 using Azure.AI.OpenAI;
 
-
 var configuration = new ConfigurationBuilder()
            .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) 
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
            .Build();
 
 string key = configuration["Azure:ApiKey"];
@@ -14,24 +13,34 @@ string deploymentName = configuration["Azure:DeploymentName"];
 string serviceHub = configuration["Azure:ServiceHub"]; // Potential use in your setup
 string projectName = configuration["Azure:ProjectName"]; // Potential use in tracking or logging
 
-
 var client = new OpenAIClient(key);
 
 ChatCompletionsOptions completionsOptions = new();
 completionsOptions.DeploymentName = deploymentName;
 
+// System message
 ChatRequestSystemMessage systemMsg =
     new(@"You are an annoyingly friendly AI 
         chatbot at the Azure Saturday AI 
         conference in Seattle. Be brief.");
-
 completionsOptions.Messages.Add(systemMsg);
-var result = client.GetChatCompletions(completionsOptions);
 
-var firstChoice = result.Value.Choices.FirstOrDefault();
+while (true)
+{
+    // AI's answer
+    Console.ForegroundColor = ConsoleColor.White;
+    var result = client.GetChatCompletions(completionsOptions);
+    var firstChoice = result.Value.Choices.FirstOrDefault();
+    var answer = firstChoice?.Message?.Content;
 
-Console.WriteLine(firstChoice?.Message?.Content ?? "No content returned");
+    completionsOptions.Messages.Add(
+        new ChatRequestAssistantMessage(answer));
+    Console.WriteLine(answer);
+    Console.WriteLine();
 
-
-
-
+    // User's Input
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    var userInput = Console.ReadLine();
+    ChatRequestUserMessage userMessage = new(userInput);
+    completionsOptions.Messages.Add(userMessage);
+}
